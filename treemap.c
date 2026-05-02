2492a646f99ab353ebb134859c19bda7bab1c2fd
Your Name <you@example.com>
2026-05-02 19:54:36 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -77,6 +77,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value)
             tree->current = tree->root;
             return;
         }
+    return NULL;
 }
 
 // 4. Implemente la función TreeNode * minimum(TreeNode * x). 
